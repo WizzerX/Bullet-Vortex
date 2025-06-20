cbeb56e46210e5e970e1542f428a5ac5c7db8995
Vpox7 <148094617+WizzerX@users.noreply.github.com>
2025-06-20 11:24:38 +0530

    init

---
+++ Bullet_Vortex/Bullet_Vortex.cpp
@@ -0,0 +1,14 @@
+#include<SFML/Graphics.hpp>
+#include<iostream>
+int main() {
+	sf::RenderWindow window(sf::VideoMode({ 1080,720 }), "GameRuning!");
+	while (window.isOpen()) {
+
+
+
+
+	}
+
+	return 0;
+
+}
